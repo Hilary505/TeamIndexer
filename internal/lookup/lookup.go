ba58bac6e1cd22c6d6bde1586f58acb10affb61c
nungo <nued65@gmail.com>
2025-03-13 17:04:02 +0300

    feat:dummy lookup fxn

--- internal/lookup/lookup.go
+++ internal/lookup/lookup.go
@@ -1,1 +1,14 @@
 package lookup
+
+func Lookup(hash string) (string, int, string) {
+	position := 0
+	stingz := ""
+	file := "abs"
+	hashmap := map[string][]byte{}
+	for key, val := range hashmap {
+		if hash == key {
+			stingz = string(val)
+		}
+	}
+	return file, position, stingz
+}
