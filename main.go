9a78ccb8942a2c1bffc3dc78644c3ac67b40dbac
Namayi <franklynenm@gmail.com>
2025-03-15 00:49:05 +0300

    add: import link

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"index/internal/chunker"
 	"index/internal/indexer"
 	"index/internal/lookup"
+	"index/internal/utils"
+
 )
 
 func main() {
@@ -40,56 +42,13 @@ func main() {
 			log.Println("Check the file path and try again")
 			return
 		}
-		indexCommand(*inputFile, *chunkSize, *indexFile)
+		utils.IndexCommand(*inputFile, *chunkSize, *indexFile)
 	case "lookup":
 		if *lookupHash == "" {
 			log.Println("SimHash value is required for lookup")
 			return
 		}
-		lookupCommand(*indexFile, *lookupHash)
+		utils.LookupCommand(*indexFile, *lookupHash)
 	}
 }
 
-/* indexCommand processes a text file and builds an index */
-func indexCommand(inputFile string, chunkSize int, indexFile string) {
-	data, err := os.ReadFile(inputFile)
-	if err != nil {
-		log.Printf("Failed to read input file: %v", err)
-		os.Exit(1)
-	}
-	chunker, err := chunker.NewChunker(chunkSize)
-	if err != nil {
-		log.Printf("Failed to create chunker: %v", err)
-		os.Exit(1)
-	}
-	chunker.Chunk(data, inputFile)
-	json.Unmarshal(data, &indexer.ChunkSlice)
-	file, err := os.Create(indexFile)
-	if err != nil {
-		log.Printf("Failed to create index file: %v", err)
-		return
-	}
-	defer file.Close()
-	jsonw, err := json.Marshal(indexer.ChunkSlice)
-	if err != nil {
-		log.Printf("%v\n", err)
-		return
-	}
-	os.WriteFile(indexFile, jsonw, 0o0644)
-	fmt.Printf("Index saved to %s\n", indexFile)
-	for simHash, chunk := range indexer.ChunkSlice {
-		fmt.Printf("Chunk ID: %d, SimHash:%v\n", chunk.ID, simHash)
-	}
-}
-
-/* lookupCommand looks up a chunk by its SimHash value */
-func lookupCommand(indexFile string, SimHash string) {
-	result, err := lookup.LookupChunkBySimHash(indexFile, SimHash)
-	if err != nil {
-		log.Fatalf("Lookup failed: %v", err)
-		return
-	}
-	fmt.Printf("Source File: %s\n", result.SourceFile)
-	fmt.Printf("Position: %d\n", result.Position)
-	fmt.Printf("Phrase: %s\n", result.Phrase)
-}
